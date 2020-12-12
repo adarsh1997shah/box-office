0ff10baf857ac31b9d8fe1de45a088a27375852f
adarsh1997shah <adarshshah01@gmail.com>
2020-12-13 01:47:07 +0530

    used reducer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
                 <Switch>
                     <Route path="/" exact component={Home}></Route>
                     <Route path="/starred" component={Starred}></Route>
+                    <Route path="/show/:showId" component={Show}></Route>
                     <Route>This page is not found</Route>
                 </Switch>
             </BrowserRouter>
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
+import { useParams } from 'react-router-dom';
+import { getResult } from '../apiGet';
+
+const initialState = {
+    showDes: null,
+    isLoading: true,
+    err: null,
+};
+
+const reducer = (prevState, action) => {
+    switch (action.type) {
+        case 'FETCH_SUCCESS':
+            return { isLoading: false, err: null, showDes: action.showDes };
+        case 'FETCH_FAILED':
+            return { ...prevState, isLoading: false, err: action.err };
+        default: {
+            return prevState;
+        }
+    }
+};
 
 const Show = () => {
+    const [{ showDes, isLoading, err }, dispatch] = useReducer(
+        reducer,
+        initialState
+    );
+
+    const { showId } = useParams();
+
+    useEffect(() => {
+        let isMounted = true;
+
+        async function fetchData() {
+            const result = await getResult(
+                'shows/1?embed[]=seasons&embed[]=cast'
+            );
+
+            if (isMounted) {
+                dispatch({ type: 'FETCH_SUCCESS', showDes: result });
+            }
+        }
+
+        fetchData().catch(err => {
+            if (isMounted) {
+                dispatch({ type: 'FETCH_SUCCESS', err: err.message });
+            }
+        });
+
+        // Unmounting.
+        return () => {
+            isMounted = false;
+        };
+    }, [showId]);
+    console.log(showDes);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (err) {
+        return <div>Sorry error Occured: {err}</div>;
+    }
+
     return (
-        <>
+        <div>
             <div>This is Show</div>
-        </>
+        </div>
     );
 };
 
